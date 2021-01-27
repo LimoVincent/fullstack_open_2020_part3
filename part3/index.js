640ad1565d25627f3ded9ad415f2a089ab064a84
Limo Vincent <48591371+LimoVincent@users.noreply.github.com>
2021-01-27 15:10:47 +0200

    step8 done!

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const PORT = 3001
 app.use(express.json())
+
+morgan.token('data', (req) => JSON.stringify(req.body))
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+)
+
 let persons = [
   {
     name: 'Arto Hellas',
